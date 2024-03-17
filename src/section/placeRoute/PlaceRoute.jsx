8361254eb75ef10eca2e5ec79f35f81bb926f973
Parth Patel <140066704+Parth002933v@users.noreply.github.com>
2024-03-17 16:00:20 +0530

    wqdj

--- src/section/placeRoute/PlaceRoute.jsx
+++ src/section/placeRoute/PlaceRoute.jsx
@@ -7,17 +7,19 @@ export default function PlaceRoute() {
   const position = [51.505, -0.09]; // Example position: [latitude, longitude]
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className='my-auto max-h-max'>
+      <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 };
 
